db6950c5df0283e9be3da6ac644cc21016a2400a
Sridhar <vadlasreedhar123@gmail.com>
2025-10-31 12:52:48 -0300

    Add Netlify SPA redirect for React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
